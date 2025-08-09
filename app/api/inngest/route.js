f37245f1ef2df744517d45fe26be7d88fc71638e
Vansh bhatt <144117143+Vanshbhattok@users.noreply.github.com>
2025-08-10 03:11:31 +0530

    Delete app/api/inngest/route.js

--- app/api/inngest/route.js
+++
@@ -1,6 +0,0 @@
-import { inngest } from "@/lib/inngest/client";
-import { serve } from "inngest/next";
-import { helloWorld } from "@/lib/inngest/functions";
-
-export const { GET, POST, PUT } = serve("InterPal App", [helloWorld]);
-// ...existing code...
